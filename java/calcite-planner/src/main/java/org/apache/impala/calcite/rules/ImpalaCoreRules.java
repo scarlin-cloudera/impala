99684b1a47e76b813882d74275c3d99d2f234c78
Steve Carlin <scarlin@cloudera.com>
2025-09-18 16:14:23 -0700

    IMPALA-14482: Calcite planner: order by "random()" fixed
    The SortRemoveConstantKeysRule in Calcite was removing functions
    that did not have any column expressions in it.
    
    This commit makes a modification to that rule to ensure that the
    order by is not optimized out for non-deterministic functions.
    
    The test for this can be found in:
    test_sort.py::TestRandomSort::test_order_by_random
    
    Change-Id: I6ebf3aa11030d98b447d08d1364f5f65eb2ca661

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaCoreRules.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaCoreRules.java
@@ -43,6 +43,7 @@ import org.apache.calcite.rel.rules.UnionPullUpConstantsRule;
 import org.apache.calcite.rel.rules.UnionToDistinctRule;
 import org.apache.calcite.rel.rules.ValuesReduceRule;
 import org.apache.calcite.rel.rules.CoreRules;
+import org.apache.calcite.rel.rules.SortRemoveConstantKeysRule;
 import org.apache.calcite.rel.rules.SubQueryRemoveRule;
 import org.apache.calcite.tools.RelBuilder;
 import org.apache.calcite.tools.RelBuilderFactory;
@@ -185,11 +186,6 @@ public class ImpalaCoreRules {
           .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
            .as(JoinToMultiJoinRule.Config.class).toRule();
 
-  public static SortRemoveConstantKeysRule SORT_REMOVE_CONSTANT_KEYS =
-      SortRemoveConstantKeysRule.Config.DEFAULT
-          .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
-           .as(SortRemoveConstantKeysRule.Config.class).toRule();
-
   // Impala defined rules
   public static RelOptRule IMPALA_MINUS_TO_DISTINCT =
       ImpalaMinusToDistinctRule.Config.DEFAULT
@@ -210,12 +206,11 @@ public class ImpalaCoreRules {
   public static RelOptRule JOIN_PROJECT_TRANSPOSE_RIGHT_OUTER =
       ImpalaJoinProjectTransposeRule.RIGHT_OUTER;
 
-  public static ImpalaLoptOptimizeJoinRule MULTI_JOIN_OPTIMIZE =
-      ImpalaLoptOptimizeJoinRule.Config.DEFAULT
-          .withSwapInputsFunction((mq, mj, left, right, sj, cond, rexB, adjust)
-              -> ImpalaLoptOptimizeJoinRule.swapInputs(mq, mj, left, right, cond,
-                  rexB, adjust))
-          .toRule();
+  public static ImpalaSortRemoveConstantKeysRule SORT_REMOVE_CONSTANT_KEYS =
+      new ImpalaSortRemoveConstantKeysRule(
+          SortRemoveConstantKeysRule.Config.DEFAULT
+              .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
+              .as(SortRemoveConstantKeysRule.Config.class));
 
   // If the straight join hint is seen, we exclude applying the JOIN_TO_MULTI_JOIN
   // rule which will ensure no join optimization is done on the join.
@@ -224,4 +219,11 @@ public class ImpalaCoreRules {
           .excludedRules(CoreRules.JOIN_TO_MULTI_JOIN)
          .build())
      .build();
+
+  public static ImpalaLoptOptimizeJoinRule MULTI_JOIN_OPTIMIZE =
+      ImpalaLoptOptimizeJoinRule.Config.DEFAULT
+          .withSwapInputsFunction((mq, mj, left, right, sj, cond, rexB, adjust)
+              -> ImpalaLoptOptimizeJoinRule.swapInputs(mq, mj, left, right, cond,
+                  rexB, adjust))
+          .toRule();
 }
