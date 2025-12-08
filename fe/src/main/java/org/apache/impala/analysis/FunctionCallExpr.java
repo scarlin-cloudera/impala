2a4ac08a04f63ddae04abda295a32ec04685d071
Steve Carlin <scarlin@cloudera.com>
2025-12-08 10:27:39 -0800

    Revert "IMPALA-13695: Calcite planner: fix for ndv with 2 args"
    This reverts commit 420e357b9586b45fbf4d08265fa57dd616989377.

--- fe/src/main/java/org/apache/impala/analysis/FunctionCallExpr.java
+++ fe/src/main/java/org/apache/impala/analysis/FunctionCallExpr.java
@@ -622,9 +622,7 @@ public class FunctionCallExpr extends Expr {
   // the needed memory for that precision value which is 2^precision.
   // This method must be identical to function ComputeHllLengthFromScale()
   // defined in aggregate-functions-ir.cc.
-  public static int ComputeHllLengthFromScale(int scale) {
-    return 1 << (scale + 8);
-  }
+  private int ComputeHllLengthFromScale(int scale) { return 1 << (scale + 8); }
 
   @Override
   protected void analyzeImpl(Analyzer analyzer) throws AnalysisException {
