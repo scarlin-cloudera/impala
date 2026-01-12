da9974088146aa834a6c3498789f8d227b51121f
Steve Carlin <scarlin@cloudera.com>
2026-01-12 07:50:39 -0800

    IMPALA-14102: [part 3] Modified LoptOptimizeJoinRule with Impala changes
    The first cut of LoptOptimizeJoinRule class was imported from
    Calcite 1.37. This had not been used yet but served as a base for
    future Impala changes on this class.
    
    This commit provides the changes that are needed for the rule to
    allow Impala specific join optimization code. The rule name has
    been changed to ImpalaLoptOptimizeJoinRule to underscore that
    this is Impala specific code.
    
    There are some basic name changes of variables, methods,
    configurations that were changed. There are two main changes
    with this commit.
    
    1) The swapInputs() method compares cost models to decide if
       the build side should be swapped with the probe side. The
       Calcite version only looks at top level row counts, but this
       produced some less optimal plans.
    2) The CumulativeCost handler is overridden to factor in that
       scan costs need to rely on only the columns fetched from the
       scan, not all of the columns.
    
    Testing: Tpcds queries have been changed. Some JUnit tests have been
    added as well.
    
    Change-Id: I168ae903934c5c74520a22605e73f71dc0a9d549

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteRelNodeConverter.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteRelNodeConverter.java
@@ -53,8 +53,10 @@ import org.apache.calcite.tools.Frameworks;
 import org.apache.calcite.tools.RelBuilder;
 import org.apache.impala.calcite.operators.ImpalaConvertletTable;
 import org.apache.impala.calcite.rules.ImpalaCoreRules;
+import org.apache.impala.calcite.rules.ImpalaMQContext;
 import org.apache.impala.calcite.rules.ImpalaRexExecutor;
 import org.apache.impala.calcite.rules.RemoveUnraggedCharCastRexExecutor;
+import org.apache.impala.calcite.schema.ImpalaCost;
 import org.apache.impala.calcite.schema.ImpalaRelMetadataProvider;
 import org.apache.impala.calcite.util.LogUtil;
 
@@ -87,7 +89,7 @@ public class CalciteRelNodeConverter implements CompilerStep {
     this.typeFactory_ = analysisResult.getTypeFactory();
     this.reader_ = analysisResult.getCatalogReader();
     this.sqlValidator_ = analysisResult.getSqlValidator();
-    this.planner_ = new VolcanoPlanner();
+    this.planner_ = new VolcanoPlanner(ImpalaCost.FACTORY, new ImpalaMQContext());
     planner_.addRelTraitDef(ConventionTraitDef.INSTANCE);
     planner_.setExecutor(new RemoveUnraggedCharCastRexExecutor());
     cluster_ =
