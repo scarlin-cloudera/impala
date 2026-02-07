904b1154d8719ca1467f0c97af2799171b538c2b
Steve Carlin <scarlin@cloudera.com>
2026-02-06 20:30:46 -0800

    unsupported complex table
    Change-Id: I26d99928fb062a89b2346ceb9db70b664407a097

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteParsedStatement.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteParsedStatement.java
@@ -28,6 +28,7 @@ import org.apache.impala.analysis.TableName;
 import org.apache.impala.analysis.StmtMetadataLoader;
 import org.apache.impala.catalog.FeCatalog;
 import org.apache.impala.common.ImpalaException;
+import org.apache.impala.common.UnsupportedFeatureException;
 import org.apache.impala.thrift.TQueryCtx;
 
 /**
@@ -54,6 +55,11 @@ public class CalciteParsedStatement implements ParsedStatement {
         new CalciteMetadataHandler.TableVisitor(queryCtx.session.database);
 
     parsedNode_.accept(tableVisitor);
+
+    if (tableVisitor.errorTables_.size() > 0) {
+      throw new UnsupportedFeatureException("Table " + tableVisitor.errorTables_.get(0)
+          + " is not supported.");
+    }
 
     tableNames_ = tableVisitor.tableNames_;
   }
