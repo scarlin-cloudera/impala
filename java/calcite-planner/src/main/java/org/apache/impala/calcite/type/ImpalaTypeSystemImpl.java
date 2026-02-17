7aa5531da2f95bd5a82b21317f5c8477db8917f0
Steve Carlin <scarlin@cloudera.com>
2026-02-17 11:37:59 -0800

    Revert "some compilation changes needed for 1.42 when it comes out"
    This reverts commit 03a536d0e706eff5d157e2e6074bea3205d0e98c.

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/type/ImpalaTypeSystemImpl.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/type/ImpalaTypeSystemImpl.java
@@ -68,7 +68,7 @@ public class ImpalaTypeSystemImpl extends RelDataTypeSystemImpl {
   public int getMaxScale(SqlTypeName typeName) {
     switch (typeName) {
     case DECIMAL:
-      return ScalarType.MAX_SCALE;
+      return getMaxNumericScale();
     case INTERVAL_YEAR:
     case INTERVAL_MONTH:
     case INTERVAL_YEAR_MONTH:
@@ -136,7 +136,7 @@ public class ImpalaTypeSystemImpl extends RelDataTypeSystemImpl {
     case VARCHAR:
       return Integer.MAX_VALUE;
     case DECIMAL:
-      return ScalarType.MAX_PRECISION;
+      return getMaxNumericPrecision();
     case INTERVAL_YEAR:
     case INTERVAL_MONTH:
     case INTERVAL_YEAR_MONTH:
@@ -166,6 +166,16 @@ public class ImpalaTypeSystemImpl extends RelDataTypeSystemImpl {
     default:
       return -1;
     }
+  }
+
+  @Override
+  public int getMaxNumericScale() {
+    return ScalarType.MAX_SCALE;
+  }
+
+  @Override
+  public int getMaxNumericPrecision() {
+    return ScalarType.MAX_PRECISION;
   }
 
   @Override
