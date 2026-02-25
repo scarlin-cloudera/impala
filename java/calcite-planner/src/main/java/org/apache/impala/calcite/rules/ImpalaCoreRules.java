245d71b99c08a240831d7d05b9ab432963a6bc0e
Steve Carlin <scarlin@cloudera.com>
2026-02-25 11:51:33 -0800

    keep project over values
    The following query failed after some change:
    
    with v1 as (
      select '0' as a1, '' as b1 from alltypestiny
    ), v2 as (
      select '' as a2, null as b2
    ), v3 as (
      select b1 as b
      from v1 left join v2 on a1 = a2
    )
    select 1 from (
      select row_number() over (partition by b order by b) rnk
      from v3
    ) v
    where rnk = 1
    
    The project above the values should be kept intact.
    
    Change-Id: I37032aa66614fc16b0667a58029bc0dfa3c4bc2b

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaCoreRules.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaCoreRules.java
@@ -135,11 +135,6 @@ public class ImpalaCoreRules {
           .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
            .as(AggregateProjectPullUpConstantsRule.Config.class).toRule();
 
-  public static FilterProjectTransposeRule FILTER_PROJECT_TRANSPOSE =
-      FilterProjectTransposeRule.Config.DEFAULT
-          .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
-           .as(FilterProjectTransposeRule.Config.class).toRule();
-
   public static FilterSetOpTransposeRule FILTER_SET_OP_TRANSPOSE =
       FilterSetOpTransposeRule.Config.DEFAULT
           .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
@@ -170,10 +165,6 @@ public class ImpalaCoreRules {
           .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
            .as(FilterMergeRule.Config.class).toRule();
 
-  public static ProjectMergeRule PROJECT_MERGE =
-      ProjectMergeRule.Config.DEFAULT
-          .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
-           .as(ProjectMergeRule.Config.class).toRule();
 
   public static JoinToMultiJoinRule JOIN_TO_MULTI_JOIN =
       JoinToMultiJoinRule.Config.DEFAULT
@@ -211,6 +202,18 @@ public class ImpalaCoreRules {
               -> ImpalaLoptOptimizeJoinRule.swapInputs(mq, mj, left, right, cond,
                   rexB, adjust))
           .toRule();
+
+  public static ImpalaFilterProjectTransposeRule FILTER_PROJECT_TRANSPOSE =
+      new ImpalaFilterProjectTransposeRule(
+          FilterProjectTransposeRule.Config.DEFAULT
+              .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
+              .as(FilterProjectTransposeRule.Config.class));
+
+  public static ImpalaProjectMergeRule PROJECT_MERGE =
+      new ImpalaProjectMergeRule(
+          ProjectMergeRule.Config.DEFAULT
+              .withRelBuilderFactory(LOGICAL_BUILDER_NO_SIMPLIFY)
+              .as(ProjectMergeRule.Config.class));
 
   // If the straight join hint is seen, we exclude applying the JOIN_TO_MULTI_JOIN
   // rule which will ensure no join optimization is done on the join.
