a4ae86eab0d4689f8d2a8a9f421f9c1c194c9867
Steve Carlin <scarlin@cloudera.com>
2026-03-07 17:01:30 -0800

    Revert "keep project over values"
    This reverts commit ba7b4eb2adb27e539643cb3e86b55e50dd42c72c.

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/rel/node/ImpalaProjectRel.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/rel/node/ImpalaProjectRel.java
@@ -209,27 +209,17 @@ public class ImpalaProjectRel extends Project
     if (RelOptUtil.InputFinder.bits(projects, null).size() > 0) {
       return false;
     }
-    ImpalaValuesRel values = getTrivialValuesNode(getInput(0));
-    if (values == null) {
+    ImpalaPlanRel relInput = (ImpalaPlanRel) getInput(0);
+    if (!(relInput instanceof ImpalaValuesRel)) {
       return false;
     }
 
+    ImpalaValuesRel values = (ImpalaValuesRel) relInput;
     if (values.getTuples().size() != 1) {
       return false;
     }
 
     return true;
-  }
-
-  private ImpalaValuesRel getTrivialValuesNode(RelNode node) {
-    if (node instanceof ImpalaValuesRel) {
-      return (ImpalaValuesRel) node;
-    }
-    if (!(node instanceof ImpalaProjectRel)) {
-      return null;
-    }
-    RelNode input = node.getInput(0);
-    return input instanceof ImpalaValuesRel ? (ImpalaValuesRel) input : null;
   }
 
   private NodeWithExprs createUnionPlanNode(ParentPlanRelContext context,
