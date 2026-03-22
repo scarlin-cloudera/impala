1edf5ef81f3781a4847b0944724ed61633f762d9
Steve Carlin <scarlin@cloudera.com>
2026-03-22 09:36:33 -0700

    get shuffle join to work, but this needs fixing
    Change-Id: Id59bb83e3169fa43734cae6491c17c71688095fa

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/rel/node/ImpalaJoinRel.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/rel/node/ImpalaJoinRel.java
@@ -113,8 +113,18 @@ public class ImpalaJoinRel extends Join
     // IMPALA-13176: TODO: Impala allows forcing hints for the distribution mode
     // - e.g force broadcast or hash partition join.  However, we are not
     // currently supporting hints from the new planner.
-    JoinNode.DistributionMode distMode = JoinNode.DistributionMode.NONE;
+    JoinNode.DistributionMode distMode = context.applyShuffleHint_
+        ? JoinNode.DistributionMode.PARTITIONED
+        : JoinNode.DistributionMode.NONE;
 
+    if (context.applyBroadcastHint_) {
+      distMode = JoinNode.DistributionMode.BROADCAST;
+      LOG.info("SJC: JOIN IS DIST MODE BROADCAST");
+    }
+
+    if (distMode == JoinNode.DistributionMode.PARTITIONED) {
+      LOG.info("SJC: JOIN IS DIST MODE PARTITIONED");
+    }
     List<ConjunctInfo> conjunctInfos = getConditionConjuncts(getCondition(),
         leftInput, rightInput, analyzer);
 
--- java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaCoreRules.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaCoreRules.java
@@ -216,6 +216,10 @@ public class ImpalaCoreRules {
      .hintStrategy("straight_join", HintStrategy.builder(HintPredicates.JOIN)
           .excludedRules(CoreRules.JOIN_TO_MULTI_JOIN)
          .build())
+     .hintStrategy("shuffle", HintStrategy.builder(HintPredicates.JOIN).build())
+     .hintStrategy("`shuffle`", HintStrategy.builder(HintPredicates.JOIN).build())
+     .hintStrategy("broadcast", HintStrategy.builder(HintPredicates.JOIN).build())
+     .hintStrategy("`broadcast`", HintStrategy.builder(HintPredicates.JOIN).build())
      .build();
 
   public static ImpalaLoptOptimizeJoinRule MULTI_JOIN_OPTIMIZE =
