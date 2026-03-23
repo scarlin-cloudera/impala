e70256826b77ddc2fa5edc361a0b5f1af19c2196
Steve Carlin <scarlin@cloudera.com>
2026-03-22 21:33:14 -0700

    Revert "Put in unwrappedSlotRef code again to fix ..."
    This reverts commit e64fd7cf9648ec3aa983788b6db0e5f69845027a.

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/functions/AnalyzedCastExpr.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/functions/AnalyzedCastExpr.java
@@ -21,27 +21,21 @@ import org.apache.impala.analysis.Analyzer;
 import org.apache.impala.analysis.CastExpr;
 import org.apache.impala.analysis.Expr;
 import org.apache.impala.analysis.StringLiteral;
-import org.apache.impala.analysis.SlotRef;
 import org.apache.impala.catalog.Type;
 import org.apache.impala.common.AnalysisException;
 
 import java.util.List;
-
 /**
  * A CastExpr that is always in analyzed state
  */
 public class AnalyzedCastExpr extends CastExpr {
 
-  private final boolean isImplicit_;
-
-  public AnalyzedCastExpr(Type targetType, List<Expr> paramList, boolean isImplicit) {
+  public AnalyzedCastExpr(Type targetType, List<Expr> paramList) {
     super(targetType, paramList.get(0).clone(), getFormat(paramList));
-    isImplicit_ = isImplicit;
   }
 
   public AnalyzedCastExpr(AnalyzedCastExpr other) {
     super(other);
-    isImplicit_ = other.isImplicit_;
   }
 
   @Override
@@ -53,17 +47,8 @@ public class AnalyzedCastExpr extends CastExpr {
   protected void analyzeImpl(Analyzer analyzer) throws AnalysisException {
   }
 
-  @Override
-  public SlotRef unwrapSlotRef(boolean implicitOnly) {
-    if (implicitOnly && !isImplicit_) {
-      return null;
-    }
-    Expr unwrappedExpr = children_.get(0);
-    return (unwrappedExpr instanceof SlotRef) ? (SlotRef) unwrappedExpr : null;
-  }
-
   /**
-   * XXX:
+   * Calcite casts will not be implicit. TODO: need to fix for the toSql routine.
    */
   @Override
   public boolean isImplicit() {
--- java/calcite-planner/src/main/java/org/apache/impala/calcite/functions/RexCallConverter.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/functions/RexCallConverter.java
@@ -271,7 +271,7 @@ public class RexCallConverter {
       throw new AnalysisException("Invalid type cast " +
           "from " + paramsOperand.getType() + " to " + impalaRetType);
     }
-    return new AnalyzedCastExpr(impalaRetType, params, call.getOperator().getKind() == SqlKind.CAST);
+    return new AnalyzedCastExpr(impalaRetType, params);
   }
 
   private static Expr createDecodeExpr(Function fn, List<Expr> params,
