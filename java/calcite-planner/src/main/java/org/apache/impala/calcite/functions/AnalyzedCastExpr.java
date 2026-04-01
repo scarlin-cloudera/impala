26c43f2bc633bb7e882d40b2904428133796a9a5
Steve Carlin <scarlin@cloudera.com>
2026-04-01 16:02:20 -0700

    Whole bunch of parsing issues
    Change-Id: Ibe4a28535cc3accf90a046001a0c9ab2c45b6418

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/functions/AnalyzedCastExpr.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/functions/AnalyzedCastExpr.java
@@ -20,16 +20,19 @@ package org.apache.impala.calcite.functions;
 import org.apache.impala.analysis.Analyzer;
 import org.apache.impala.analysis.CastExpr;
 import org.apache.impala.analysis.Expr;
+import org.apache.impala.analysis.StringLiteral;
 import org.apache.impala.catalog.Type;
 import org.apache.impala.common.AnalysisException;
+
+import java.util.List;
 
 /**
  * A CastExpr that is always in analyzed state
  */
 public class AnalyzedCastExpr extends CastExpr {
 
-  public AnalyzedCastExpr(Type targetType, Expr e) {
-    super(targetType, e.clone());
+  public AnalyzedCastExpr(Type targetType, List<Expr> paramList, boolean isImplicit) {
+    super(targetType, paramList.get(0).clone(), getFormat(paramList));
   }
 
   public AnalyzedCastExpr(AnalyzedCastExpr other) {
@@ -51,5 +54,11 @@ public class AnalyzedCastExpr extends CastExpr {
   @Override
   public boolean isImplicit() {
     return false;
+  }
+
+  private static String getFormat(List<Expr> paramsList) {
+    return paramsList.size() == 1
+        ? null
+        : ((StringLiteral)paramsList.get(1)).getStringValue();
   }
 }
