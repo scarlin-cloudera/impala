54174c25ad54257b31151b91b598c0bcb0434d6d
Steve Carlin <scarlin@cloudera.com>
2026-04-01 09:14:56 -0700

    DEFAULT FALLBACK PLANNER IS CALCITE
    Change-Id: Ic9f3db58152bcc3241360abcf61de159e47a5b65

--- common/thrift/Query.thrift
+++ common/thrift/Query.thrift
@@ -813,10 +813,10 @@ struct TQueryOptions {
   199: optional i32 show_create_table_partition_limit = 1000
 
   // See comment in ImpalaService.thrift
-  200: optional PlanNodes.TPlannerType planner = TPlannerType.ORIGINAL
+  200: optional PlanNodes.TPlannerType planner = TPlannerType.CALCITE
 
   // See comment in ImpalaService.thrift
-  201: optional PlanNodes.TPlannerType fallback_planner = TPlannerType.ORIGINAL
+  201: optional PlanNodes.TPlannerType fallback_planner = TPlannerType.CALCITE
 }
 
 // Impala currently has three types of sessions: Beeswax, HiveServer2 and external
