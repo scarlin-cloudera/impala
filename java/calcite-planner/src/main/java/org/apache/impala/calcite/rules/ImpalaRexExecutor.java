0f9c0967ba1a9be8991be3bdce3135cbc1e8a0f4
Steve Carlin <scarlin@cloudera.com>
2026-04-01 18:01:44 -0700

    IMPALA-14910: Calcite planner: support expressions in limit clause
    This commit contains changes to the Calcite planner which support
    expressions in the limit clause, e.g. "limit 1 + 1"
    
    The parser now has the capability of reading expressions in the clause.
    
    The validator was changed to force a BIGINT return type for whatever
    expression exists in the limit clause.
    
    There are a few rules in Calcite which expect a RexLiteral to be in
    the "LogicalSort.fetch" and "LogicalSort.offset" fields. The reduction
    logic is therefore done before most rules are applied. If the expression
    is unable to be simplified to a RexLiteral, an exception will be thrown,
    but this is really no different from current behavior if the SQL
    contained something like "limit <col1>".
    
    The reduction logic for the expression is in the ImpalaSortSimplifyRule
    which is similar to the ImpalaFilterSimplifyRule (and Project and Join)
    
    The normal reduction logic tries to keep the RelDataType unchanged and
    adds a cast if necessary to do so. However, the expression in the limit
    cannot handle a cast or a change in nullability. So some logic was added
    to avoid this when the expression is in a limit clause.
    
    Change-Id: Ic01a0144671485156654e7685d0d5816fa743f9d

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaRexExecutor.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/rules/ImpalaRexExecutor.java
@@ -61,17 +61,31 @@ public class ImpalaRexExecutor implements RexExecutor {
   // Reducer class used for testing purposes for injection.
   private final Reducer reducer_;
 
+  // shouldCast_ is false if the type of the variable pre-reduction does not need
+  // to match the variable post-reduction. Note: A change in the nullability of
+  // the datatype is also avoided if this variable is false.
+  private final boolean shouldCast_;
+
   public ImpalaRexExecutor(CalciteAnalysisResult analysisResult, Reducer reducer) {
-    analyzer_ = analysisResult.getAnalyzer();
-    queryCtx_ = analyzer_.getQueryCtx();
-    reducer_ = reducer;
+    this(analysisResult.getAnalyzer(), analysisResult.getAnalyzer().getQueryCtx(),
+        reducer, true);
   }
 
   public ImpalaRexExecutor(Analyzer analyzer, TQueryCtx queryCtx,
       Reducer reducer) {
+    this(analyzer, queryCtx, reducer, true);
+  }
+
+  public ImpalaRexExecutor(ImpalaRexExecutor executor, boolean shouldCast) {
+    this(executor.analyzer_, executor.queryCtx_, executor.reducer_, shouldCast);
+  }
+
+  public ImpalaRexExecutor(Analyzer analyzer, TQueryCtx queryCtx, Reducer reducer,
+      boolean shouldCast) {
     analyzer_ = analyzer;
     queryCtx_ = queryCtx;
     reducer_ = reducer;
+    shouldCast_ = shouldCast;
   }
 
   /**
@@ -87,7 +101,7 @@ public class ImpalaRexExecutor implements RexExecutor {
         new ReduceRexNodeShuttle(analyzer_, queryCtx_, rexBuilder, reducer_);
     for (RexNode rexNode : constExps) {
       RexNode newProject = rexNode.accept(shuttle);
-      if (rexNode.getType() != newProject.getType()) {
+      if (rexNode.getType() != newProject.getType() && shouldCast_) {
         newProject = rexBuilder.makeCast(rexNode.getType(), newProject,
             rexNode.getType().isNullable());
       }
