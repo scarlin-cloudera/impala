22283cfc550a6f281b0ef7df75d28f4bb4d7086b
Steve Carlin <scarlin@cloudera.com>
2026-04-02 09:31:33 -0700

    Revert "column stats to union col descriptors"
    This reverts commit 71e0304f99735400c4f3a0897f4b88cc583f5f63.

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/rel/node/NodeCreationUtils.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/rel/node/NodeCreationUtils.java
@@ -21,11 +21,9 @@ import com.google.common.collect.ImmutableList;
 
 import org.apache.impala.analysis.Analyzer;
 import org.apache.impala.analysis.Expr;
-import org.apache.impala.analysis.SetOperationStmt;
 import org.apache.impala.analysis.SlotDescriptor;
 import org.apache.impala.analysis.SlotRef;
 import org.apache.impala.analysis.TupleDescriptor;
-import org.apache.impala.catalog.ColumnStats;
 import org.apache.impala.planner.EmptySetNode;
 import org.apache.impala.planner.PlanNodeId;
 import org.apache.impala.planner.SelectNode;
@@ -39,7 +37,6 @@ import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.rex.RexNode;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class NodeCreationUtils {
 
@@ -79,12 +76,8 @@ public class NodeCreationUtils {
   public static NodeWithExprs createUnionPlanNode(PlanNodeId nodeId,
       Analyzer analyzer, RelDataType rowType, List<NodeWithExprs> childrenPlanNodes,
       boolean unionAll) throws ImpalaException {
-    List<List<Expr>> inputExprsList = childrenPlanNodes.stream()
-        .map(c -> c.outputExprs_).collect(Collectors.toList());
-    List<ColumnStats> columnStats = SetOperationStmt.getColumnStats(inputExprsList);
-    TupleDescriptorFactory tupleDescFactory = new TupleDescriptorFactory("union",
-        rowType, columnStats.size() > 0 ? columnStats : null);
-        new TupleDescriptorFactory("union", rowType, columnStats);
+    TupleDescriptorFactory tupleDescFactory =
+        new TupleDescriptorFactory("union", rowType);
     TupleDescriptor tupleDesc = tupleDescFactory.create(analyzer);
     // The outputexprs are the SlotRef exprs passed to the parent node.
     List<Expr> outputExprs = createOutputExprs(tupleDesc.getSlots());
