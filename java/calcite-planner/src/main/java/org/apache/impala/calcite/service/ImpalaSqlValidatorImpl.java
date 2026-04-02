693096ce9e281e7a3b476be634fb5ed1687c9a28
Steve Carlin <scarlin@cloudera.com>
2026-04-01 21:01:35 -0700

    stuff for string/char
    ..but this is broken!  tpdcs 5 is not working!
    
    Change-Id: I61e7c496ce0cafef3fb7e2911a12e85ad3ce5b82

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/ImpalaSqlValidatorImpl.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/ImpalaSqlValidatorImpl.java
@@ -41,6 +41,7 @@ import org.apache.calcite.sql.validate.SqlValidatorUtil;
 import org.apache.calcite.sql.SqlOperatorTable;
 import org.apache.calcite.sql.SqlBasicCall;
 import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlCharStringLiteral;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.SqlIdentifier;
 import org.apache.calcite.sql.SqlJoin;
@@ -61,6 +62,7 @@ import org.apache.impala.catalog.BuiltinsDb;
 import org.apache.impala.catalog.FeView;
 import org.apache.impala.catalog.FeFsTable;
 import org.apache.impala.common.UnsupportedFeatureException;
+import org.apache.impala.catalog.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -115,6 +117,11 @@ public class ImpalaSqlValidatorImpl extends SqlValidatorImpl {
         setValidatedNodeType(operand, type);
         return type;
       }
+    }
+    if (operand instanceof SqlCharStringLiteral) {
+      RelDataType type = ImpalaTypeConverter.getRelDataType(Type.STRING, false);
+      setValidatedNodeType(operand, type);
+      return type;
     }
     return super.deriveType(scope, operand);
   }
