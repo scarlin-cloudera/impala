ad23677a80c9ef3916f81e9c37d38546f3424f43
Steve Carlin <scarlin@cloudera.com>
2026-04-01 20:30:00 -0700

    error handling stuff
    Change-Id: I20f38408fafbbd6b66ef0e6e4a8c6cfbc9f79b8e

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteMetadataHandler.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteMetadataHandler.java
@@ -32,6 +32,7 @@ import org.apache.calcite.sql.SqlJoin;
 import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlSelect;
+import org.apache.calcite.sql.SqlSnapshot;
 import org.apache.calcite.sql.SqlWith;
 import org.apache.calcite.sql.SqlWithItem;
 import org.apache.calcite.sql.util.SqlBasicVisitor;
@@ -46,6 +47,7 @@ import org.apache.impala.calcite.util.SimplifiedAnalyzer;
 import org.apache.impala.catalog.FeCatalog;
 import org.apache.impala.catalog.FeDb;
 import org.apache.impala.catalog.FeTable;
+import org.apache.impala.catalog.IcebergTable;
 import org.apache.impala.common.ImpalaException;
 import org.apache.impala.common.UnsupportedFeatureException;
 import org.apache.impala.thrift.TQueryCtx;
@@ -116,7 +118,15 @@ public class CalciteMetadataHandler {
       FeTable feTable = db.getTable(tableName.getTbl());
       if (feTable == null) {
         notFoundTables.add(tableName.toString());
+        CalciteDb.Builder dbBuilder =
+            dbSchemas.getOrDefault(tableName.getDb(), new CalciteDb.Builder(reader));
+        dbSchemas.put(tableName.getDb().toLowerCase(), dbBuilder);
         continue;
+      }
+
+      if (feTable instanceof IcebergTable) {
+        throw new UnsupportedFeatureException("Iceberg tables not supported " +
+            "with Calcite Planner.");
       }
 
       // populate the dbschema with its table, creating the dbschema if it's the
@@ -152,7 +162,9 @@ public class CalciteMetadataHandler {
     // the top of the stack.
     public final Stack<Set<TableName>> withItemTableNames_ = new Stack<>();
 
-    public TableVisitor(String currentDb) {
+    public ImpalaException exception_ = null;
+
+    private TableVisitor(String currentDb) {
       this.currentDb_ = currentDb.toLowerCase();
     }
 
@@ -190,6 +202,10 @@ public class CalciteMetadataHandler {
     }
 
     private List<TableName> getTableNames(SqlNode fromNode) {
+      if (fromNode instanceof SqlSnapshot) {
+        return getTableNames(((SqlSnapshot) fromNode).getTableRef());
+      }
+
       List<TableName> localTableNames = new ArrayList<>();
       if (fromNode instanceof SqlIdentifier) {
         String tableName = fromNode.toString();
@@ -207,7 +223,8 @@ public class CalciteMetadataHandler {
           localTableNames.add(
               new TableName(parts.get(0).toLowerCase(), parts.get(1).toLowerCase()));
         } else {
-          errorTables_.add(tableName);
+          exception_ = new UnsupportedFeatureException(
+              "Table " + tableName + " is not supported.");
           return localTableNames;
         }
       }
@@ -224,6 +241,10 @@ public class CalciteMetadataHandler {
         if (basicCall.getKind().equals(SqlKind.AS)) {
           localTableNames.addAll(getTableNames(basicCall.operand(0)));
         }
+        if (basicCall.getKind() == SqlKind.UNNEST) {
+           exception_ = new UnsupportedFeatureException(
+               "Unnest function is not supported at this time.");
+        }
       }
       return localTableNames;
     }
@@ -233,6 +254,17 @@ public class CalciteMetadataHandler {
         if (tableNames.contains(tableName)) return true;
       }
       return false;
+    }
+
+    public static Set<TableName> getTableNames(SqlNode sqlNode, String db)
+        throws ImpalaException {
+      
+      TableVisitor tableVisitor = new TableVisitor(db);
+      sqlNode.accept(tableVisitor);
+      if (tableVisitor.exception_ != null) {
+        throw tableVisitor.exception_;
+      }
+      return tableVisitor.tableNames_;
     }
   }
 
@@ -245,5 +277,14 @@ public class CalciteMetadataHandler {
       }
     }
     return false;
+  }
+
+  public static boolean isIcebergTable(TQueryCtx queryCtx, Analyzer analyzer,
+      StmtMetadataLoader.StmtTableCache stmtTableCache, String db, String tableName) {
+    try {
+      return stmtTableCache.catalog.getTable(db, tableName) != null;
+    } catch (Exception e) {
+      return false;
+    }
   }
 }
