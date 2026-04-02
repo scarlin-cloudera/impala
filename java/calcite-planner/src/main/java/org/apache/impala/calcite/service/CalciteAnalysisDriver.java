b349a9445b16be0ccb0ec73fe64675535e401f61
Steve Carlin <scarlin@cloudera.com>
2026-04-01 20:30:00 -0700

    IMPALA-14944: Throw UnsupportedFeatureException for some Calcite planner queries
    This commit adds code to throw an UnsupportedFeatureException for queries
    that cannot be handled by the Calcite planner. The benefit for throwing this
    specific exception rather than any other exception is that this will be
    used to track queries not supported by Calcite and allow them to pass by
    running them through the original planner. In this way, when all queries
    are either supported or have this exception thrown, the Jenkins job will
    be able to pass when there is no fallback planner specified.
    
    This commit includes:
    
    - Some syntax supported by Impala has been added to the Parser.jj file.
    A ParseException is thrown and the message is caught an the Unsupported exception
    is then rethrown. This includes keywords like "tablesample", "unnest", "row__id",
    and "for system_version as of".
    
    - Backticks around column names are caught and rethrown.
    
    - If an InconsistentMetadataFetchException is thrown, this is because there
    was some metadata that changed after a fetch. This exception is now passed
    through to the top level and the query will be retried in Calcite again. While
    this doesn't directly pertain to the spirit of this commit, it has been added
    because it is another case of retrying a query that has failed.
    
    - The CreateExprVisitor has been changed to rethrow a found error. Previous to
    this commit, it could not handle throwing an ImpalaException and would just
    throw a RuntimeException.
    
    - The CalciteMetadataHandler has been changed to allow Unsupported exceptions
    to be thrown. The TableVisitor has been wrapped in a static getTableNames
    method, and this static method handles the rethrow of the exception.
    
    - Iceberg tables are specifically caught and thrown as Unsupported.
    
    - There is a specific type of "with" query that is supported by Impala but
    not supported by Calcite:
        with t(c1) as (select int_col, bigint_col from alltypestiny)
        select * from t order by c1 limit 1
    This query has one column defined in "with t(c1)", but 2 columns "(int_col,
    bigint_col)" defined in the inner select. Calcite flags this as a validation
    error, but some code has been added to catch when this situation happens
    and throw the Unsupported error so that the original planner can run it.
    
    - Some extra syntax has been added for catching when tables are not found.
    Sometimes the error shows up as "Object ... not found"
    
    - Some extra code in the authorization area was needed to deal with some
    of these Unsupported issues. If a table is not found, it needs to be checked
    to see if it authorized. This way, a proper unauthorized error message can
    be thrown whether the table is there or not. The problem that Calcite has is
    that it does not have the ability at this point to differentiate between
    <db>.<table_name> and <table_name>.<complex_column>.  Rather than try to
    authorize within the Calcite planner, the authorization will be skipped and
    the Unsupported error will be thrown. This allows the original planner to
    do the proper checking and then send an ambiguous authorization failure
    whether the table exists or not.
    
    Testing: All these situations exist with the test framework and have been
    tested when the fallback planner is turned off and the original planner
    is Calcite.
    
    Change-Id: I20f38408fafbbd6b66ef0e6e4a8c6cfbc9f79b8e

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisDriver.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisDriver.java
@@ -160,14 +160,19 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
       return new CalciteAnalysisResult(this);
     } catch (ImpalaException e) {
       try {
-        UnsupportedChecker.throwUnsupportedIfKnownException(e, stmtTableCache_);
+        UnsupportedChecker.throwUnsupportedIfKnownException(e, stmtTableCache_,
+            queryCtx_, analyzer_);
       } catch (ImpalaException u) {
         e = u;
       }
       return new CalciteAnalysisResult(this, e);
     } catch (CalciteContextException e) {
+      if (e.getCause() instanceof UnsupportedFeatureException) {
+        return new CalciteAnalysisResult(this, (UnsupportedFeatureException)e.getCause());
+      }
       try {
-        UnsupportedChecker.throwUnsupportedIfKnownException(e, stmtTableCache_);
+        UnsupportedChecker.throwUnsupportedIfKnownException(e, stmtTableCache_,
+            queryCtx_, analyzer_);
       } catch (ImpalaException u) {
         return new CalciteAnalysisResult(this, u);
       }
@@ -221,9 +226,8 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
         String sql = ((FeView) feTable).getQueryStmt().toSql();
         CalciteQueryParser queryParser = new CalciteQueryParser(sql);
         SqlNode parsedSqlNode = queryParser.parse();
-        CalciteMetadataHandler.TableVisitor tableVisitor =
-            new CalciteMetadataHandler.TableVisitor(/* currentDb */ "default");
-        parsedSqlNode.accept(tableVisitor);
+        Set<TableName> tableNames = CalciteMetadataHandler.TableVisitor.getTableNames(
+            parsedSqlNode, "default");
 
         boolean childViewCreatedBySuperuser =
             !PrivilegeRequestBuilder.isViewCreatedByNonSuperuser(feTable);
@@ -255,7 +259,7 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
 
         // Recurse if 'feTable' is also a view. Note that the privilege requests for the
         // tables referenced by 'feTable' will be registered within the recursive call.
-        registerPrivReqsInTables(tableVisitor.tableNames_,
+        registerPrivReqsInTables(tableNames,
             shouldMaskPrivChecks || childViewCreatedBySuperuser, catalog, validator);
 
         // Set 'maskPrivChecks_' back to false in this case because we do not know if
--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteMetadataHandler.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteMetadataHandler.java
@@ -31,6 +31,7 @@ import org.apache.calcite.sql.SqlJoin;
 import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlSelect;
+import org.apache.calcite.sql.SqlSnapshot;
 import org.apache.calcite.sql.SqlWith;
 import org.apache.calcite.sql.SqlWithItem;
 import org.apache.calcite.sql.util.SqlBasicVisitor;
@@ -46,6 +47,7 @@ import org.apache.impala.calcite.util.SimplifiedAnalyzer;
 import org.apache.impala.catalog.FeCatalog;
 import org.apache.impala.catalog.FeDb;
 import org.apache.impala.catalog.FeTable;
+import org.apache.impala.catalog.IcebergTable;
 import org.apache.impala.common.ImpalaException;
 import org.apache.impala.common.UnsupportedFeatureException;
 import org.apache.impala.thrift.TQueryCtx;
@@ -98,7 +100,7 @@ public class CalciteMetadataHandler {
    * Populate the CalciteSchema with tables being used by this query. Returns a
    * list of tables in the query that are not found in the database.
    */
-  public static List<String> populateCalciteSchema(CalciteCatalogReader reader,
+  public static void populateCalciteSchema(CalciteCatalogReader reader,
       FeCatalog catalog, StmtMetadataLoader.StmtTableCache stmtTableCache,
       Analyzer analyzer) throws ImpalaException {
     List<String> notFoundTables = new ArrayList<>();
@@ -108,15 +110,18 @@ public class CalciteMetadataHandler {
       FeDb db = catalog.getDb(tableName.getDb());
       // db is not found, this will probably fail in the validation step
       if (db == null) {
-        notFoundTables.add(tableName.toString());
         continue;
       }
 
       // table is not found, this will probably fail in the validation step
       FeTable feTable = db.getTable(tableName.getTbl());
       if (feTable == null) {
-        notFoundTables.add(tableName.toString());
         continue;
+      }
+
+      if (feTable instanceof IcebergTable) {
+        throw new UnsupportedFeatureException("Iceberg tables not supported " +
+            "with Calcite Planner.");
       }
 
       // populate the dbschema with its table, creating the dbschema if it's the
@@ -131,7 +136,6 @@ public class CalciteMetadataHandler {
     for (String dbName : dbSchemas.keySet()) {
       rootSchema.add(dbName, dbSchemas.get(dbName.toLowerCase()).build());
     }
-    return notFoundTables;
   }
 
   /**
@@ -152,7 +156,9 @@ public class CalciteMetadataHandler {
     // the top of the stack.
     public final Stack<Set<TableName>> withItemTableNames_ = new Stack<>();
 
-    public TableVisitor(String currentDb) {
+    public ImpalaException exception_ = null;
+
+    private TableVisitor(String currentDb) {
       this.currentDb_ = currentDb.toLowerCase();
     }
 
@@ -190,6 +196,11 @@ public class CalciteMetadataHandler {
     }
 
     private List<TableName> getTableNames(SqlNode fromNode) {
+      // Iceberg tables can sometimes be found under the SqlSnapshot node.
+      if (fromNode instanceof SqlSnapshot) {
+        return getTableNames(((SqlSnapshot) fromNode).getTableRef());
+      }
+
       List<TableName> localTableNames = new ArrayList<>();
       if (fromNode instanceof SqlIdentifier) {
         String tableName = fromNode.toString();
@@ -207,7 +218,8 @@ public class CalciteMetadataHandler {
           localTableNames.add(
               new TableName(parts.get(0).toLowerCase(), parts.get(1).toLowerCase()));
         } else {
-          errorTables_.add(tableName);
+          exception_ = new UnsupportedFeatureException(
+              "Table " + tableName + " is not supported.");
           return localTableNames;
         }
       }
@@ -224,6 +236,10 @@ public class CalciteMetadataHandler {
         if (basicCall.getKind().equals(SqlKind.AS)) {
           localTableNames.addAll(getTableNames(basicCall.operand(0)));
         }
+        if (basicCall.getKind() == SqlKind.UNNEST) {
+           exception_ = new UnsupportedFeatureException(
+               "Unnest function is not supported at this time.");
+        }
       }
       return localTableNames;
     }
@@ -233,6 +249,17 @@ public class CalciteMetadataHandler {
         if (tableNames.contains(tableName)) return true;
       }
       return false;
+    }
+
+    public static Set<TableName> getTableNames(SqlNode sqlNode, String db)
+        throws ImpalaException {
+
+      TableVisitor tableVisitor = new TableVisitor(db);
+      sqlNode.accept(tableVisitor);
+      if (tableVisitor.exception_ != null) {
+        throw tableVisitor.exception_;
+      }
+      return tableVisitor.tableNames_;
     }
   }
 
@@ -245,5 +272,14 @@ public class CalciteMetadataHandler {
       }
     }
     return false;
+  }
+
+  public static boolean isTableInCache(TQueryCtx queryCtx, Analyzer analyzer,
+      StmtMetadataLoader.StmtTableCache stmtTableCache, String db, String tableName) {
+    try {
+      return stmtTableCache.catalog.getTable(db, tableName) != null;
+    } catch (Exception e) {
+      return false;
+    }
   }
 }
