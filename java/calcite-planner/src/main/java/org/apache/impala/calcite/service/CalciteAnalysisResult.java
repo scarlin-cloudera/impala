b183e46584452375a07955d44e70313585fa6ef0
Steve Carlin <scarlin@cloudera.com>
2026-04-01 20:30:00 -0700

    error handling stuff
    Change-Id: I20f38408fafbbd6b66ef0e6e4a8c6cfbc9f79b8e

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisResult.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisResult.java
@@ -24,6 +24,7 @@ import org.apache.calcite.sql.validate.SqlValidator;
 import org.apache.impala.analysis.AnalysisContext.AnalysisResult;
 import org.apache.impala.analysis.AnalysisDriver;
 import org.apache.impala.common.ImpalaException;
+import org.apache.impala.common.UnsupportedFeatureException;
 
 /**
  * CalciteAnalysisResult is an AnalysisResult with added analysis result
@@ -70,5 +71,19 @@ public class CalciteAnalysisResult extends AnalysisResult {
     return typeFactory_;
   }
 
+  @Override
+  public boolean shouldFallbackBecauseOfException() {
+    return getException() instanceof UnsupportedFeatureException;
+  }
+
+  public static CalciteAnalysisResult createValidAnalysisResult(CalciteAnalysisDriver a,
+      ImpalaException potentialException) {
+    return new CalciteAnalysisResult(a, null, potentialException);
+  }
+
+  public static CalciteAnalysisResult createErrorAnalysisResult(CalciteAnalysisDriver a,
+      ImpalaException exception) {
+    return new CalciteAnalysisResult(a, exception, null);
+  }
 
 }
