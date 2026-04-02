d8328525ce928f29c5476456b59aecb9e65f7045
Steve Carlin <scarlin@cloudera.com>
2026-04-01 19:51:23 -0700

    view fixing
    Change-Id: I8d9901c0ae8cbc903b38c575dbb08257f034687d

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisDriver.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisDriver.java
@@ -50,6 +50,7 @@ import org.apache.impala.authorization.Privilege;
 import org.apache.impala.authorization.PrivilegeRequestBuilder;
 import org.apache.impala.calcite.operators.ImpalaOperatorTable;
 import org.apache.impala.calcite.schema.ImpalaCalciteCatalogReader;
+import org.apache.impala.calcite.schema.ImpalaViewTable;
 import org.apache.impala.calcite.type.ImpalaTypeCoercionFactory;
 import org.apache.impala.calcite.type.ImpalaTypeFactoryImpl;
 import org.apache.impala.calcite.type.ImpalaTypeSystemImpl;
@@ -68,6 +69,7 @@ import org.apache.impala.planner.PlannerContext;
 import org.apache.impala.planner.SingleNodePlannerIntf;
 import org.apache.impala.thrift.TQueryCtx;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
 /**
@@ -175,7 +177,8 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
         analyzer_.registerPrivReq(builder -> builder.allOf(Privilege.VIEW_METADATA)
             .onDb(ctx_.getCatalog().getDb(fnName.getDb())).build());
       }
-      return new CalciteAnalysisResult(this);
+      return CalciteAnalysisResult.createValidAnalysisResult(this,
+          sqlValidator_.getPossibleValidationException());
     } catch (ImpalaException e) {
       try {
         UnsupportedChecker.throwUnsupportedIfKnownException(e, stmtTableCache_,
@@ -183,7 +186,7 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
       } catch (ImpalaException u) {
         e = u;
       }
-      return new CalciteAnalysisResult(this, e);
+      return CalciteAnalysisResult.createErrorAnalysisResult(this, e);
     } catch (CalciteContextException e) {
       if (e.getCause() instanceof UnsupportedFeatureException) {
         return CalciteAnalysisResult.createErrorAnalysisResult(this,
@@ -197,9 +200,9 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
         UnsupportedChecker.throwUnsupportedIfKnownException(e, stmtTableCache_,
             queryCtx_, analyzer_);
       } catch (ImpalaException u) {
-        return new CalciteAnalysisResult(this, u);
+        return CalciteAnalysisResult.createErrorAnalysisResult(this, u);
       }
-      return new CalciteAnalysisResult(this,
+      return CalciteAnalysisResult.createErrorAnalysisResult(this,
           new AnalysisException(e.getMessage(), e.getCause()));
     }
   }
@@ -240,7 +243,7 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
    */
   private void registerPrivReqsInTables(Set<TableName> tableNamesInQuery,
       boolean shouldMaskPrivChecks, FeCatalog catalog, ImpalaSqlValidatorImpl validator)
-      throws ParseException {
+      throws ParseException, ImpalaException {
 
     for (TableName tableName : tableNamesInQuery) {
       FeTable feTable = registerTablePrivReq(tableName, catalog);
@@ -271,7 +274,12 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
           analyzer_.setMaskPrivChecks(null);
         }
         // Register privilege requests for columns referenced by the child view.
-        validator.validate(parsedSqlNode);
+        parsedSqlNode = validate(validator, parsedSqlNode, queryParser);
+
+        ImpalaViewTable calciteView =
+            reader_.getTable(tableName.toPath()).unwrap(ImpalaViewTable.class);
+        Preconditions.checkNotNull(calciteView);
+        calciteView.setValidatedNode(parsedSqlNode);
 
         // Recurse if 'feTable' is also a view. Note that the privilege requests for the
         // tables referenced by 'feTable' will be registered within the recursive call.
@@ -283,6 +291,26 @@ public class CalciteAnalysisDriver implements AnalysisDriver {
         if (!shouldMaskPrivChecks && childViewCreatedBySuperuser) {
           analyzer_.unsetMaskPrivChecks();
         }
+      }
+    }
+  }
+
+  private SqlNode validate(ImpalaSqlValidatorImpl validator, SqlNode parsedSqlNode,
+      CalciteQueryParser queryParser) throws ParseException, ImpalaException {
+    try {
+      validator.startValidatingView(false);
+      parsedSqlNode = validator.validate(parsedSqlNode);
+      validator.endValidatingView();
+      return parsedSqlNode;
+    } catch (Exception e) { //XXX: have a more specific exception here
+      if (validator.foundAliasIssue()) {
+        parsedSqlNode = queryParser.parse();
+        validator.startValidatingView(true);
+        parsedSqlNode = validator.validate(parsedSqlNode);
+        validator.endValidatingView();
+        return parsedSqlNode;
+      } else {
+        throw e;
       }
     }
   }
--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisResult.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteAnalysisResult.java
@@ -42,17 +42,16 @@ public class CalciteAnalysisResult extends AnalysisResult {
   // may be needed by the CalciteTable object
   private final CalciteCatalogReader reader_;
 
-  public CalciteAnalysisResult(CalciteAnalysisDriver analysisDriver) {
-    this(analysisDriver, null);
-  }
+  public final ImpalaException potentialException_;
 
-  public CalciteAnalysisResult(CalciteAnalysisDriver analysisDriver,
-      ImpalaException e) {
-    super(analysisDriver.getParsedStmt(), analysisDriver.getAnalyzer(), e);
+  private CalciteAnalysisResult(CalciteAnalysisDriver analysisDriver,
+      ImpalaException exception, ImpalaException potentialException) {
+    super(analysisDriver.getParsedStmt(), analysisDriver.getAnalyzer(), exception);
     validatedNode_ = analysisDriver.getValidatedNode();
     typeFactory_ = analysisDriver.getTypeFactory();
     sqlValidator_ = analysisDriver.getSqlValidator();
     reader_ = analysisDriver.getCatalogReader();
+    potentialException_ = potentialException;
   }
 
   public CalciteCatalogReader getCatalogReader() {
@@ -74,5 +73,15 @@ public class CalciteAnalysisResult extends AnalysisResult {
   @Override
   public boolean shouldFallbackBecauseOfException() {
     return getException() instanceof UnsupportedFeatureException;
+  }
+
+  public static CalciteAnalysisResult createValidAnalysisResult(CalciteAnalysisDriver a,
+      ImpalaException potentialException) {
+    return new CalciteAnalysisResult(a, null, potentialException);
+  }
+
+  public static CalciteAnalysisResult createErrorAnalysisResult(CalciteAnalysisDriver a,
+      ImpalaException exception) {
+    return new CalciteAnalysisResult(a, exception, null);
   }
 }
