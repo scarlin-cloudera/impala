288f7caa49999cd8395448874444ef0d7ab1e43d
Steve Carlin <scarlin@cloudera.com>
2026-04-01 21:25:22 -0700

    stuff for string/char (2)
    Change-Id: Id5624071d55dc085b6e8f2643179471170337097

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/ImpalaSqlValidatorImpl.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/ImpalaSqlValidatorImpl.java
@@ -40,6 +40,7 @@ import org.apache.calcite.sql.validate.SqlValidatorUtil;
 import org.apache.calcite.sql.SqlOperatorTable;
 import org.apache.calcite.sql.SqlBasicCall;
 import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlCharStringLiteral;
 import org.apache.calcite.sql.SqlFunction;
 import org.apache.calcite.sql.SqlIdentifier;
 import org.apache.calcite.sql.SqlKind;
@@ -57,6 +58,7 @@ import org.apache.impala.calcite.type.ImpalaTypeConverter;
 import org.apache.impala.catalog.BuiltinsDb;
 import org.apache.impala.catalog.FeView;
 import org.apache.impala.catalog.FeFsTable;
+import org.apache.impala.catalog.Type;
 import org.apache.impala.common.UnsupportedFeatureException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -112,6 +114,11 @@ public class ImpalaSqlValidatorImpl extends SqlValidatorImpl {
         setValidatedNodeType(operand, type);
         return type;
       }
+    }
+    if (operand instanceof SqlCharStringLiteral) {
+      RelDataType type = ImpalaTypeConverter.getRelDataType(Type.STRING, false);
+      setValidatedNodeType(operand, type);
+      return type;
     }
     return super.deriveType(scope, operand);
   }
