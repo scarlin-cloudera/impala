600c2d8e0ae9427e9d5c7de23f6a4d7404e6af18
Steve Carlin <scarlin@cloudera.com>
2026-04-17 15:39:16 -0700

    Revert "added makeCast which allows this query to work for removing union rettype changes"
    This reverts commit 61b07d192818205a0a9d52a7ab7f842f40ac8384.

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/operators/ImpalaRexBuilder.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/operators/ImpalaRexBuilder.java
@@ -20,8 +20,6 @@ import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.rex.RexLiteral;
-import org.apache.calcite.rex.RexNode;
-import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.calcite.sql.type.SqlTypeUtil;
 import org.apache.impala.catalog.Type;
@@ -81,22 +79,5 @@ public class ImpalaRexBuilder extends RexBuilder {
     }
 
     return super.makeLiteral(o, type, typeName);
-  }
-
-  @Override
-  public RexNode makeCast(
-      SqlParserPos pos,
-      RelDataType type,
-      RexNode exp,
-      boolean matchNullability,
-      boolean safe,
-      RexLiteral format) {
-    if (exp instanceof RexLiteral) {
-      RexLiteral literal = (RexLiteral) exp;
-      if (!literal.isNull() && SqlTypeUtil.isIntType(type) && !type.equals(literal.getType())) {
-        return makeAbstractCast(pos, type, literal, safe, format);
-      }
-    }
-    return super.makeCast(pos, type, exp, matchNullability, safe, format);
   }
 }
