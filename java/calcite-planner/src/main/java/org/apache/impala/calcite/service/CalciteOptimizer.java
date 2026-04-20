75fccfa1ebea35445f4422ae47f9a8a3d8dd6664
Steve Carlin <scarlin@cloudera.com>
2026-04-20 05:58:34 -0700

    Test
    Change-Id: Id69cc72f221f0abe14fb819cf8e848694430098c

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteOptimizer.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteOptimizer.java
@@ -191,8 +191,9 @@ public class CalciteOptimizer implements CompilerStep {
         ImpalaCoreRules.FILTER_AGGREGATE_TRANSPOSE,
         ImpalaCoreRules.UNION_REMOVE,
         ImpalaCoreRules.PROJECT_TO_SEMI_JOIN,
-        ImpalaCoreRules.FILTER_VALUES_MERGE,
-        ImpalaCoreRules.PROJECT_VALUES_MERGE,
+        // CALCITE-7450: ValuesReduceRule is buggy.  Fixed in 1.42
+        // ImpalaCoreRules.FILTER_VALUES_MERGE,
+        // ImpalaCoreRules.PROJECT_VALUES_MERGE,
         ImpalaCoreRules.FILTER_MERGE,
         ImpalaCoreRules.PROJECT_MERGE,
         ImpalaCoreRules.JOIN_PUSH_EXPRESSIONS,
