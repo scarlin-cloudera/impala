b2835d736bd4b27440763f9932d893b1fd5d0092
Steve Carlin <scarlin@cloudera.com>
2026-06-12 15:28:57 -0700

    Revert "anti join stuff"
    This reverts commit ee3b2cca1c4759e9304c4de27b742dee953b6e00.

--- java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteOptimizer.java
+++ java/calcite-planner/src/main/java/org/apache/impala/calcite/service/CalciteOptimizer.java
@@ -155,7 +155,6 @@ public class CalciteOptimizer implements CompilerStep {
 
     builder.addMatchOrder(HepMatchOrder.BOTTOM_UP);
     builder.addRuleCollection(ImmutableList.of(
-        ImpalaCoreRules.MINUS_TO_ANTIJOIN,
         ImpalaCoreRules.INTERSECT_TO_DISTINCT,
         ImpalaCoreRules.UNION_TO_DISTINCT,
         ImpalaCoreRules.IMPALA_MINUS_TO_DISTINCT,
