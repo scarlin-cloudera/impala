4034d93529cf847a89be6a6249f3f355ec78a0f7
Steve Carlin <scarlin@cloudera.com>
2026-06-26 16:21:02 -0700

    test
    Change-Id: Id6c2f61c05cc9bba35a54ca671ee98e785581c6f

--- common/thrift/Query.thrift
+++ common/thrift/Query.thrift
@@ -813,7 +813,7 @@ struct TQueryOptions {
   199: optional i32 show_create_table_partition_limit = 1000
 
   // See comment in ImpalaService.thrift
-  200: optional PlanNodes.TPlannerType planner = TPlannerType.ORIGINAL
+  200: optional PlanNodes.TPlannerType planner = TPlannerType.CALCITE
 
   // See comment in ImpalaService.thrift
   201: optional PlanNodes.TPlannerType fallback_planner = TPlannerType.ORIGINAL
